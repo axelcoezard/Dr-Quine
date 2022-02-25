0e2c709ceee728c5e6c98fcc2f14f6a255f1a076
Axel Coézard <axel.coezard@gmail.com>
2022-02-25 10:04:53 +0100

    opti de Grace

--- Grace_kid.c
+++ Grace_kid.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define FD "Grace_kid.c"
-#define CODE "#include <stdio.h>%c%c#define FD %c%s%c%c#define CODE %c%s%c%c#define MAIN(x) int main(void) { FILE* fd = fopen(FD, x); fprintf(fd, CODE, 10, 10, 34, FD, 34, 10, 34, CODE, 34, 10, 10, 34, x, 34, 10, 10); return 0; }%cMAIN(%c%s%c)%c/* COM */%c"
-#define MAIN(x) int main(void) { FILE* fd = fopen(FD, x); fprintf(fd, CODE, 10, 10, 34, FD, 34, 10, 34, CODE, 34, 10, 10, 34, x, 34, 10, 10); return 0; }
+#define CODE "#include <stdio.h>%c%c#define FD %c%s%c%c#define CODE %c%s%c%c#define MAIN(x) int main(void) { fprintf(fopen(FD, x), CODE, 10, 10, 34, FD, 34, 10, 34, CODE, 34, 10, 10, 34, x, 34, 10, 10); return 0; }%cMAIN(%c%s%c)%c/* COM */%c"
+#define MAIN(x) int main(void) { fprintf(fopen(FD, x), CODE, 10, 10, 34, FD, 34, 10, 34, CODE, 34, 10, 10, 34, x, 34, 10, 10); return 0; }
 MAIN("w")
 /* COM */
