fecb6667fc86863823649db04139e98cb8f8e899
Axel Coézard <axel.coezard@gmail.com>
2022-02-25 10:14:39 +0100

    fix Colleen norm

--- C/Colleen.c
+++ C/Colleen.c
@@ -4,9 +4,13 @@ void other(void) {}
 
 int main(void)
 {
-	char* code = "#include <stdio.h>%c%cvoid other(void) {}%c%cint main(void)%c{%c%cchar* code = %c%s%c;%c%cprintf(code, 10, 10, 10, 10, 10, 10, 9, 34, code, 34, 10, 9, 10, 9, 10, 9, 10, 10, 10);%c%creturn 0;%c%c/* COM 1 */%c}%c/* COM 2 */%c";
-	printf(code, 10, 10, 10, 10, 10, 10, 9, 34, code, 34, 10, 9, 10, 9, 10, 9, 10, 10, 10);
+	char* code = "#include <stdio.h>%c%cvoid other(void) {}%c%cint main(void)%c{%c%cchar* code = %c%s%c;%c%cprintf(code, 10, 10, 10, 10, 10, 10, 9, 34, code, 34, 10, 9, 10, 9, 10, 9, 10, 9, 9, 10, 9, 10, 10, 10, 9, 10, 10);%c%creturn 0;%c%c/*%c%c%cCOM 1%c%c*/%c}%c/*%c%cCOM 2%c*/%c";
+	printf(code, 10, 10, 10, 10, 10, 10, 9, 34, code, 34, 10, 9, 10, 9, 10, 9, 10, 9, 9, 10, 9, 10, 10, 10, 9, 10, 10);
 	return 0;
-	/* COM 1 */
+	/*
+		COM 1
+	*/
 }
-/* COM 2 */
+/*
+	COM 2
+*/
--- Grace_kid.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-#define FD "Grace_kid.c"
-#define CODE "#include <stdio.h>%c%c#define FD %c%s%c%c#define CODE %c%s%c%c#define MAIN(x) int main(void) { fprintf(fopen(FD, x), CODE, 10, 10, 34, FD, 34, 10, 34, CODE, 34, 10, 10, 34, x, 34, 10, 10); return 0; }%cMAIN(%c%s%c)%c/* COM */%c"
-#define MAIN(x) int main(void) { fprintf(fopen(FD, x), CODE, 10, 10, 34, FD, 34, 10, 34, CODE, 34, 10, 10, 34, x, 34, 10, 10); return 0; }
-MAIN("w")
-/* COM */
