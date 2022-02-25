e74162224e258e495387596705b4b84911aae0a9
Axel Coézard <axel.coezard@gmail.com>
2022-02-25 12:52:23 +0100

    finish C part

--- C/Sully.c
+++ C/Sully.c
@@ -5,7 +5,7 @@
 
 int main(void)
 {
-char* code = "#include <stdio.h>%c#include <stdlib.h>%c%c#define N %d%c%cint main(void)%c{%cchar* code = %c%s%c;%cchar *fo, *fc, *compile, *execute;%casprintf(&fo, %cSully_%c%c%c, N - 1);%casprintf(&fc, %cSully_%c%c.c%c, N - 1);%casprintf(&compile, %cgcc -o %c%c %c%c%c, fo, fc);%casprintf(&execute, %c./%c%c%c, fo);%cFILE* fd = fopen(fc, %cw%c);%cfprintf(fd, code, 10, 10, 10, N - 1, 10, 10, 10, 10, 34, code, 34, 10, 10, 34, 37, 100, 34, 10, 34, 37, 100, 34, 10, 34, 37, 115, 37, 115, 34, 10, 34, 37, 115, 34, 10, 34, 34, 10, 10, 10, 10, 10, 10, 10);%cfclose(fd);%csystem(compile);%cif (N > 0) system(execute);%creturn 0;%c}%c";
+char* code = "#include <stdio.h>%c#include <stdlib.h>%c%c#define N %d%c%cint main(void)%c{%cchar* code = %c%s%c;%cchar *fo, *fc, *compile, *execute;%casprintf(&fo, %cSully_%c%c%c, N - 1);%casprintf(&fc, %cSully_%c%c.c%c, N - 1);%casprintf(&compile, %cgcc -o %c%c %c%c%c, fo, fc);%casprintf(&execute, %c./%c%c%c, fo);%cFILE* fd = fopen(fc, %cw%c);%cfprintf(fd, code, 10, 10, 10, N - 1, 10, 10, 10, 10, 34, code, 34, 10, 10, 34, 37, 100, 34, 10, 34, 37, 100, 34, 10, 34, 37, 115, 37, 115, 34, 10, 34, 37, 115, 34, 10, 34, 34, 10, 10, 10, 10, 10, 10, 10);%cfclose(fd);%csystem(compile);%cif (N > 1) system(execute);%creturn 0;%c}%c";
 char *fo, *fc, *compile, *execute;
 asprintf(&fo, "Sully_%d", N - 1);
 asprintf(&fc, "Sully_%d.c", N - 1);
@@ -15,7 +15,7 @@ FILE* fd = fopen(fc, "w");
 fprintf(fd, code, 10, 10, 10, N - 1, 10, 10, 10, 10, 34, code, 34, 10, 10, 34, 37, 100, 34, 10, 34, 37, 100, 34, 10, 34, 37, 115, 37, 115, 34, 10, 34, 37, 115, 34, 10, 34, 34, 10, 10, 10, 10, 10, 10, 10);
 fclose(fd);
 system(compile);
-if (N > 0) system(execute);
+if (N > 1) system(execute);
 return 0;
 }
 
