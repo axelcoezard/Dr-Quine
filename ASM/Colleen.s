32367eff66caa6e873e2e2583585e374d1ff15a0
Axel Coézard <axel.coezard@gmail.com>
2022-03-24 15:01:10 +0100

    ajout de Colleen.asm

---
+++ ASM/Colleen.s
@@ -0,0 +1,15 @@
+section .data
+code: db "section .data%1$ccode: db %2$c%3$s%2$c, 0%1$csection .text%1$cextern _printf%1$cglobal _main%1$c_main:%1$cpush rbp%1$cmov rbp, rsp%1$clea rdi, [rel code]%1$cmov rsi, 0xA%1$cmov rdx, 0x22%1$clea rcx, [rel code]%1$ccall _printf%1$cleave%1$cret%1$c", 0
+section .text
+extern _printf
+global _main
+_main:
+push rbp
+mov rbp, rsp
+lea rdi, [rel code]
+mov rsi, 0xA
+mov rdx, 0x22
+lea rcx, [rel code]
+call _printf
+leave
+ret
