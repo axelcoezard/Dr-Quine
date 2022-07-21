378523ffe8b2dc0363e66a3cffe66f78944d5fb6
Axel Coézard <axel.coezard@gmail.com>
2022-07-21 09:41:14 +0200

    fix sully when negative (JS)

--- JS/Sully.js
+++ JS/Sully.js
@@ -2,6 +2,7 @@
 	const fs = require("fs")
 	const process = require("child_process")
 	let N = 5
+	if (N <= 0) return;
 	if (fs.existsSync(`Sully_${N}.js`)) N--;
 	fs.appendFileSync(`Sully_${N}.js`, `(${main.toString().replace(/\d+/, N)})()\n`)
 	if (N > 0) process.execFile("node", [`Sully_${N}.js`])
